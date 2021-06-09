1b81f2596baf5613ed319ea55adbeaaaa1fc0102
Miracutor <miracleexecutors@gmail.com>
2021-06-10 03:29:00 +0800

    Fix corrections

--- actions/actions.py
+++ actions/actions.py
@@ -6,6 +6,7 @@
 
 
 import sqlite3
+
 # This is a simple example for a custom action which utters "Hello World!"
 # from typing import Any, Text, Dict, List
 #
@@ -32,22 +33,24 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-databasePath = Path('.', 'knowledge', 'fushigi.db')
+databasePath = Path(".", "knowledge", "fushigi.db")
 conn = sqlite3.connect(databasePath)
 
 
 class ActionAnswerDirection(Action):
-
     def name(self) -> Text:
         return "action_answer_direction"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
 
         cursor = conn.cursor()
         try:
-            cursor.execute('select * from Direction')
+            cursor.execute("select * from Direction")
             direction = cursor.fetchall()
         except sqlite3.Error as e:
             print(f"The error '{e}' occurred")
@@ -64,10 +67,12 @@ class ActionAnswerDirection(Action):
                 break
 
         if result is None:
-            dispatcher.utter_message(text="Requested direction not recorded in the system.")
+            dispatcher.utter_message(
+                text="Requested direction not recorded in the system."
+            )
         else:
-            message = "Sure! To go to {}, you need to {}".format(result[1].capitalize(), result[2])
-            sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', message)
+            message = f"Sure! To go to {result[1].title()}, you need to {result[2]}"
+            sentences = re.split(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s", message)
             for s in sentences:
                 dispatcher.utter_message(s)
 
@@ -75,22 +80,26 @@ class ActionAnswerDirection(Action):
 
 
 class ActionAnswerExplain(Action):
-
     def name(self) -> Text:
         return "action_answer_explain"
 
     async def run(
-            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
 
         cursor = conn.cursor()
         try:
-            cursor.execute('select * from Explain')
+            cursor.execute("select * from Explain")
             explain = cursor.fetchall()
         except sqlite3.Error as e:
             print(f"The error '{e}' occurred")
 
-        explain_subject: str = next(tracker.get_latest_entity_values("explain_subject"), None)
+        explain_subject: str = next(
+            tracker.get_latest_entity_values("explain_subject"), None
+        )
         # print("Explain: ["+explain_subject+"]")
 
         result = None
@@ -101,28 +110,34 @@ class ActionAnswerExplain(Action):
                 result = e
                 break
 
-        dispatcher.utter_message(text=result[2])
+        sentences = re.split(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s", result[2])
+        for s in sentences:
+            dispatcher.utter_message(s)
 
         return []
 
 
 class ActionAnswerInquiry(Action):
-
     def name(self) -> Text:
         return "action_answer_inquiry"
 
     async def run(
-            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
 
         cursor = conn.cursor()
         try:
-            cursor.execute('select * from Inquiry')
+            cursor.execute("select * from Inquiry")
             inquiry = cursor.fetchall()
         except sqlite3.Error as e:
             print(f"The error '{e}' occurred")
 
-        inquiry_subject: str = next(tracker.get_latest_entity_values("inquiry_subject"), None)
+        inquiry_subject: str = next(
+            tracker.get_latest_entity_values("inquiry_subject"), None
+        )
         # print("Inquiry: ["+inquiry_subject+"]")
 
         result = None
@@ -133,6 +148,8 @@ class ActionAnswerInquiry(Action):
                 result = i
                 break
 
-        dispatcher.utter_message(text=result[2])
+        sentences = re.split(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s", result[2])
+        for s in sentences:
+            dispatcher.utter_message(s)
 
         return []
